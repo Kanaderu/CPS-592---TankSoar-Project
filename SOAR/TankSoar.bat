82093b1b70670c59efad27eb6f4dba9cbd78540f
Unknown <fand.kanade@gmail.com>
2018-11-24 22:24:35 -0500

    Updated Self Motivated Behavior Configuration and productions

--- SOAR/TankSoar.bat
+++ SOAR/TankSoar.bat
@@ -2,5 +2,5 @@
 set SOAR_HOME=%~dp0bin
 set PATH=%SOAR_HOME%;%PATH%
 
-start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_fallacyfallacy_test.cnf
-#start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_selfmotivated_test.cnf
+#start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_fallacyfallacy_test.cnf
+start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_selfmotivated_test.cnf
