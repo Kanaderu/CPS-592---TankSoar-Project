cb28190fa0c40371dbb7c7d05c3a8ab816357137
Unknown <fand.kanade@gmail.com>
2018-11-25 11:19:16 -0500

    Fixed fallacy fallacy test

--- SOAR/TankSoar.bat
+++ SOAR/TankSoar.bat
@@ -1,6 +1,7 @@
-
 set SOAR_HOME=%~dp0bin
 set PATH=%SOAR_HOME%;%PATH%
 
+
+:: Set below the following configuration to test
 start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_fallacyfallacy_test.cnf
-#start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_selfmotivated_test.cnf
+::start javaw -Djava.library.path=%SOAR_HOME% -jar bin\Eaters_TankSoar.jar tanksoar_selfmotivated_test.cnf
